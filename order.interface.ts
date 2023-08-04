0a7cea1c4624536cf1515aac14b549d32c6be2dc
dajhelyan <assiaxramx10@gmail.com>
2023-08-04 17:30:00 -0500

    Add order interface and mongodb model

---
+++ order.interface.ts
@@ -0,0 +1,12 @@
+export interface Order {
+  id: string,
+  name: string,
+  lastName: string,
+  dni: number,
+  email: string,
+  phoneNumber: number,
+  // province: string,
+  // district: string,
+  // reference: string,
+  company: 'BBC' | 'IBK' | 'VABB' | 'SKOTI'
+}
