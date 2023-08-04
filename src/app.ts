39b419b49e034139388a2f87b958cf14ae3c5d57
dajhelyan <assiaxramx10@gmail.com>
2023-08-04 17:30:59 -0500

    Separate mongodb conection from app file

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
+import dbConection from "../config/mongo-db"
+const apiRoutes = require('../routes/orders')
+import bodyParser from 'body-parser'
+// import cors from 'cors'
 const express = require('express')
-const mongoose =require('mongoose')
-require("dotenv").config()
+// const mongoose =require('mongoose')
 
 const app = express()
 app.use(express.json())
-
+// app.use(cors)
 const PORT = process.env.PORT
 
 // db connection
-mongoose
-.connect(process.env.URI)
-.then(() => console.log('db conected'))
-.catch((error) => console.error(error))
+dbConection()
+app.use('/api', apiRoutes);
+app.use(bodyParser.json())
 
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
