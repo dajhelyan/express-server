b9b1946f1cbe052d0aea90f608007122f2a083f4
dajhelyan <assiaxramx10@gmail.com>
2023-08-10 10:19:17 -0500

    Generate jwt token after sign in

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,27 +1,45 @@
-import { User } from "../interfaces/user.interface";
+// import { log } from "console";
+import { IUser, User } from '../interfaces/user.interface';
 import userModel from "../model/user-model";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
-// import { errorHandler } from "../utils/error.handler";
+import 'dotenv/config'
 
-export async function signUp(user:User) {
+export interface IPayload {
+  userId: string
+}
+
+export async function signUp(user: User) {
   const hash = await bcrypt.hash(user.password, 10)
-  
+
   const newUser = new userModel({
     userName: user.userName,
     email: user.email,
-    password : hash
+    password: hash
   })
-  
+
   return await userModel.create(newUser)
 }
 
-export async function getUser(email:string) {
-  return await userModel.findOne({ email: email})
+export async function signIn(user: IUser) {
+
+  const existedUser = await getUser(user.email)
+  console.log(existedUser, 'user');
+
+  // desencryp password
+  const validInfo = await bcrypt.compare(user.password, existedUser?.password)
+
+  if(validInfo){ 
+    const userToken = await generateToken(existedUser?.id)
+    return userToken
+  }
 }
 
-export async function generateToken(payload:User) {
-  return jwt.sign(
-      payload    
-    )
+export async function getUser(email: string) {
+  return await userModel.findOne({ email: email })
+}
+
+export async function generateToken(payload: any) {
+  return jwt.sign(payload, process.env.TOKEN_SECRET
+  )
 }
