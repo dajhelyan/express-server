5728899fda88c4e113330f0609646c209485181d
dajhelyan <assiaxramx10@gmail.com>
2023-08-18 18:40:47 -0500

    Add headers for order req

--- client/src/api/auth.jsx
+++ client/src/api/auth.jsx
@@ -1,8 +1,48 @@
-import axios from 'axios'
+import axios, { AxiosHeaders } from "axios";
+import { useAuth } from "../authContext";
 
-const API = 'http://localhost:3000/api'
+const API = "http://localhost:3000/api";
+// const { activeUser } = useAuth();
+
+const headers = axios.create({
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Accept: "application/json",
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+    rossDomain: true,
+  },
+});
+
+// headers.interceptors.request.use(async(config) => {
+//   const value = await localStorage.getItem("token");
+//   if (value !== null) {
+//     config.defaults.headers.common["Authorization"] = `Bearer ${value}`;
+//   }
+
+//   return config;
+
+// });
 
 export const logIn = (user) => {
   console.log(user);
-  return axios.post(`${API}/login`, user)
-}
+  return axios.post(`${API}/login`, user);
+};
+
+export const validateToken = (token) => {
+  console.log(token, "token");
+  return axios.post(`${API}/verify`, token);
+};
+
+export const addOrder = async (order) => {
+  try {
+    await axios.post(`${API}/order`, order, {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        crossDomain: true,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
