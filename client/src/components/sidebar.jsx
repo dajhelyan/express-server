917a91b8c07f3a0394fac98771eab8414157acbb
dajhelyan <assiaxramx10@gmail.com>
2023-08-21 19:00:08 -0500

    Fix sidebar style

--- client/src/components/sidebar.jsx
+++ client/src/components/sidebar.jsx
@@ -11,67 +11,70 @@ import {
   Avatar,
   Typography,
   Container,
+  Paper,
 } from "@mui/material";
 
 import InboxIcon from "@mui/icons-material/Inbox";
 import LocalShippingIcon from "@mui/icons-material/LocalShipping";
 
+
+const routeDetail = [
+  {
+    text: "Crear orden",
+    icon: <LocalShippingIcon color="primary" />,
+    keys: "new order",
+    path: `/order`,
+  },
+  {
+    text: "Ver ordenes",
+    icon: <InboxIcon color="primary" />,
+    keys: "orders",
+    path: `/`,
+  },
+];
+
 export const Sidebar = () => {
-  return(
-    <Box
-    sx={{
-      // backgroundColor:'primary',
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      flexShrink: 0,
-      "& .MuiDrawer-paper": {
-        position: "relative",
-        height: "100vh",
-        boxSizing: "border-box",
-      },
-    }}
-  >
+
+  return (
     <Drawer
-    color="primary"
+      component={Paper}
+      color="primary"
       variant="permanent"
       ModalProps={{
         keepMounted: false,
       }}
       open={true}
     >
-      <Container sx={{padding:3}}>
-        <Grid container spacing={4} sx={{textAlign:'center'}}>
+      <Container sx={{ padding: 3 }}>
+        <Grid container spacing={4} sx={{ textAlign: "center" }}>
           <Grid item xs={12}>
             <Avatar
               alt="User Shoop"
               src="/static/images/avatar/1.jpg"
-              sx={{ width: 65, height: 65, margin: '0 auto' }}
+              sx={{ width: 65, height: 65, margin: "0 auto" }}
             />
           </Grid>
           <Grid item xs={12}>
-            <Typography variant="h6" >User Shoop</Typography>
+            <Typography variant="h6">User Shoop</Typography>
           </Grid>
         </Grid>
       </Container>
       <Divider light />
       <List>
-        {["Add Order", "Orders"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton color="primary">
-              <ListItemIcon>
-                {index % 2 === 0 ? (
-                  <InboxIcon color="primary" />
-                ) : (
-                  <LocalShippingIcon color="primary"/>
-                )}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+        {routeDetail.map((item, i) => {
+          return (
+            <ListItem
+              disablePadding
+            >
+              <ListItemButton key={i +1} to={item.path} selected={location.pathname === item.path} color="primary">
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
       </List>
     </Drawer>
-  </Box>
-  )
-}
+    // </Box>
+  );
+};
