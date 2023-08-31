3c88f60c15b5b7c2bc4fca5267dbef7c58cc02e0
dajhelyan <assiaxramx10@gmail.com>
2023-08-30 21:31:45 -0500

    Add order crud

--- src/services/order-service.ts
+++ src/services/order-service.ts
@@ -1,10 +1,24 @@
-import orderModel from '../model/order-model'
-import { Order } from '../order.interface'
+import orderModel from "../model/order-model";
+import { Order } from "../order.interface";
 
-export async function insertOrder (order:Order) {
-  return await orderModel.create(order)
+export async function insert(order: Order) {
+  return await orderModel.create(order);
 }
 
-export async function getOrder () {
-  return await orderModel.find({})
+export async function getAll(userId: string) {
+  return await orderModel.find({user: userId});
+}
+
+export async function getById(id: string) {
+  return await orderModel.findById(id);
+}
+
+export async function update(id: string, order: Order) {
+  return await orderModel.findByIdAndUpdate(id, order, {
+    new: true
+  });
+}
+
+export async function remove(id: string) {
+  return await orderModel.findByIdAndDelete(id)
 }
