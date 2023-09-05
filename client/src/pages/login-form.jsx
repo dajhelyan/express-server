90ca440075f89016a4c63b915df17c46e48a475e
dajhelyan <assiaxramx10@gmail.com>
2023-09-04 23:57:04 -0500

    Add table component

--- client/src/pages/login-form.jsx
+++ client/src/pages/login-form.jsx
@@ -1,15 +1,15 @@
-import {Container, Card, CardContent, Typography} from '@mui/material/'
+import {Container, Card, CardContent, Typography, Box} from '@mui/material/'
 import { Form } from '../components/form-component'
 
 export const LoginForm = () => {
   return (
-    <Container>
+    <Box>
       <Card style={{padding: 30}}>
         <Typography variant='h4'>Inicia sesion</Typography>
         <CardContent >
           <Form/>
         </CardContent>
       </Card>
-    </Container>
+    </Box>
   )
 }
