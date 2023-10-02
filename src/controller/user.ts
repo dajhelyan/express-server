5659d07df5f47d18f7ed8ece800814f9fb9c9b04
Dajhely <dangles@ftb.pe>
2023-10-02 02:29:22 -0500

    Fix duplicated user by email

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,33 +1,46 @@
 import {Request, Response} from 'express'
 import { errorHandler } from '../utils/error.handler'
-import { signIn, signUp } from '../services/user-service'
+import {  getUser, signIn, signUp } from '../services/user-service'
 
 export async function register({body}: Request, res: Response) {
-  const usersaved = await signUp(body)
-  try {
-    res.json({
-      id: usersaved.id,
-      email: usersaved.email,
-      userName: usersaved.userName
+  const existedEmail = await getUser(body.email)
+  console.log(existedEmail, 'existed email?');
+  
+  let user: any;
+  if (!existedEmail) {
+    user = await signUp(body)  
+  }
+
+  try {    
+    return res.json({
+      id: user.id,
+      email: user.email,
+      userName: user.userName
     })
-  } catch (error) {
-    errorHandler(res, error)
+  } catch (error) {    
+    return res.status(404).json({
+      message: 'Something went wrong creating user.'
+    })
   }
 }
 
 export async function logIn({body}: Request, res: Response) {
   
   // generate a token
+  // handle error response [to-d]
   try {
     const token = await signIn(body)
+
     res.json({ userToken: token })
   } catch (error) {
+    console.log(error);
+    
     errorHandler(res, error)
   }
   // return the token 
 }
 
-// export async function signOut(params:type) {
-  
-// }
-
+export async function verifyToken(req:Request,_res:Response) {
+  const validToken = req.body;
+  return validToken;
+}
